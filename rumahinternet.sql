6fc55b10806eb65640cb9279d1130f8d59591b95
djendraa <57910787+djendraa@users.noreply.github.com>
2020-07-06 15:32:46 +0700

    Add files via upload

---
+++ rumahinternet.sql
@@ -0,0 +1,430 @@
+-- phpMyAdmin SQL Dump
+-- version 5.0.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Jul 06, 2020 at 10:27 AM
+-- Server version: 10.4.11-MariaDB
+-- PHP Version: 7.2.31
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `rumahinternet`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `client`
+--
+
+CREATE TABLE `client` (
+  `id` bigint(20) NOT NULL,
+  `clientname` varchar(25) DEFAULT NULL,
+  `address` varchar(200) DEFAULT NULL,
+  `contact` varchar(15) DEFAULT NULL,
+  `created_at` datetime DEFAULT current_timestamp(),
+  `updated_at` datetime DEFAULT NULL ON UPDATE current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `detailitem`
+--
+
+CREATE TABLE `detailitem` (
+  `id` bigint(20) NOT NULL,
+  `detailitem` varchar(25) DEFAULT NULL,
+  `itemstatus_id` bigint(20) DEFAULT NULL,
+  `qty` int(11) DEFAULT NULL,
+  `instalation_id` bigint(20) DEFAULT NULL,
+  `created_at` datetime DEFAULT current_timestamp(),
+  `updated_at` datetime DEFAULT NULL ON UPDATE current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `instalation`
+--
+
+CREATE TABLE `instalation` (
+  `id` bigint(20) NOT NULL,
+  `instalation` varchar(25) DEFAULT NULL,
+  `client_id` bigint(20) DEFAULT NULL,
+  `package_id` bigint(20) DEFAULT NULL,
+  `support_id` bigint(20) DEFAULT NULL,
+  `storage_id` bigint(20) DEFAULT NULL,
+  `statusinstalation_id` bigint(20) DEFAULT NULL,
+  `created_at` datetime DEFAULT current_timestamp(),
+  `updated_at` datetime DEFAULT NULL ON UPDATE current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `item`
+--
+
+CREATE TABLE `item` (
+  `id` bigint(20) NOT NULL,
+  `item` varchar(25) DEFAULT NULL,
+  `created_at` datetime DEFAULT current_timestamp(),
+  `updated_at` datetime DEFAULT NULL ON UPDATE current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `itemstatus`
+--
+
+CREATE TABLE `itemstatus` (
+  `id` bigint(20) NOT NULL,
+  `itemstatus` varchar(25) DEFAULT NULL,
+  `created_at` datetime DEFAULT current_timestamp(),
+  `updated_at` datetime DEFAULT NULL ON UPDATE current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `orders`
+--
+
+CREATE TABLE `orders` (
+  `id` bigint(20) NOT NULL,
+  `orders` varchar(25) DEFAULT NULL,
+  `requests_id` bigint(20) DEFAULT NULL,
+  `created_at` datetime DEFAULT current_timestamp(),
+  `updated_at` datetime DEFAULT NULL ON UPDATE current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `package`
+--
+
+CREATE TABLE `package` (
+  `id` bigint(20) NOT NULL,
+  `package` varchar(25) DEFAULT NULL,
+  `created_at` datetime DEFAULT current_timestamp(),
+  `updated_at` datetime DEFAULT NULL ON UPDATE current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `purchase`
+--
+
+CREATE TABLE `purchase` (
+  `id` bigint(20) NOT NULL,
+  `purchase` varchar(25) DEFAULT NULL,
+  `store` varchar(200) DEFAULT NULL,
+  `item_id` bigint(20) DEFAULT NULL,
+  `price` int(11) DEFAULT NULL,
+  `qty` int(11) DEFAULT NULL,
+  `orders_id` bigint(20) DEFAULT NULL,
+  `statusorders_id` bigint(20) DEFAULT NULL,
+  `created_at` datetime DEFAULT current_timestamp(),
+  `updated_at` datetime DEFAULT NULL ON UPDATE current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `requests`
+--
+
+CREATE TABLE `requests` (
+  `id` bigint(20) NOT NULL,
+  `requests` varchar(25) DEFAULT NULL,
+  `qty` int(11) DEFAULT NULL,
+  `item_id` bigint(20) DEFAULT NULL,
+  `support_id` bigint(20) DEFAULT NULL,
+  `created_at` datetime DEFAULT current_timestamp(),
+  `updated_at` datetime DEFAULT NULL ON UPDATE current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `statusinstalation`
+--
+
+CREATE TABLE `statusinstalation` (
+  `id` bigint(20) NOT NULL,
+  `statusinstalation` varchar(15) DEFAULT NULL,
+  `created_at` datetime DEFAULT current_timestamp(),
+  `updated_at` datetime DEFAULT NULL ON UPDATE current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `statusorders`
+--
+
+CREATE TABLE `statusorders` (
+  `id` bigint(20) NOT NULL,
+  `statusorders` varchar(25) DEFAULT NULL,
+  `created_at` datetime DEFAULT current_timestamp(),
+  `updated_at` datetime DEFAULT NULL ON UPDATE current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `storage`
+--
+
+CREATE TABLE `storage` (
+  `id` bigint(20) NOT NULL,
+  `item_id` bigint(20) DEFAULT NULL,
+  `tipestorage_id` bigint(20) DEFAULT NULL,
+  `purchase_id` bigint(20) DEFAULT NULL,
+  `qty` int(11) DEFAULT NULL,
+  `created_at` datetime DEFAULT NULL,
+  `updated_at` datetime DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `support`
+--
+
+CREATE TABLE `support` (
+  `id` bigint(20) NOT NULL,
+  `support` varchar(25) DEFAULT NULL,
+  `user_id` bigint(20) DEFAULT NULL,
+  `created_at` datetime DEFAULT current_timestamp(),
+  `updated_at` datetime DEFAULT NULL ON UPDATE current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tipestorage`
+--
+
+CREATE TABLE `tipestorage` (
+  `id` bigint(20) NOT NULL,
+  `tipestorage` varchar(25) DEFAULT NULL,
+  `created_at` datetime DEFAULT current_timestamp(),
+  `updated_at` datetime DEFAULT NULL ON UPDATE current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tipeuser`
+--
+
+CREATE TABLE `tipeuser` (
+  `id` bigint(20) NOT NULL,
+  `tipeuser` varchar(10) DEFAULT NULL,
+  `created_at` datetime DEFAULT current_timestamp(),
+  `updated_at` datetime DEFAULT NULL ON UPDATE current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `client`
+--
+ALTER TABLE `client`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `detailitem`
+--
+ALTER TABLE `detailitem`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `FK_detailitem_itemstatus_id` (`itemstatus_id`),
+  ADD KEY `FK_detailitem_instalation_id` (`instalation_id`);
+
+--
+-- Indexes for table `instalation`
+--
+ALTER TABLE `instalation`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `FK_instalation_client_id` (`client_id`),
+  ADD KEY `FK_instalation_package_id` (`package_id`),
+  ADD KEY `FK_instalation_support_id` (`support_id`),
+  ADD KEY `FK_instalation_storage_id` (`storage_id`),
+  ADD KEY `FK_instalation_statusinstalation_id` (`statusinstalation_id`);
+
+--
+-- Indexes for table `item`
+--
+ALTER TABLE `item`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `itemstatus`
+--
+ALTER TABLE `itemstatus`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `orders`
+--
+ALTER TABLE `orders`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `FK_orders_requests_id` (`requests_id`);
+
+--
+-- Indexes for table `package`
+--
+ALTER TABLE `package`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `purchase`
+--
+ALTER TABLE `purchase`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `FK_purchase_item_id` (`item_id`),
+  ADD KEY `FK_purchase_orders_id` (`orders_id`),
+  ADD KEY `FK_purchase_statusorder_id` (`statusorders_id`);
+
+--
+-- Indexes for table `requests`
+--
+ALTER TABLE `requests`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `FK_requests_item_id` (`item_id`),
+  ADD KEY `FK_requests_support_id` (`support_id`);
+
+--
+-- Indexes for table `statusinstalation`
+--
+ALTER TABLE `statusinstalation`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `statusorders`
+--
+ALTER TABLE `statusorders`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `storage`
+--
+ALTER TABLE `storage`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `FK_storage_item_id` (`item_id`),
+  ADD KEY `FK_storage_tipestorage_id` (`tipestorage_id`),
+  ADD KEY `FK_storage_purchase_id` (`purchase_id`);
+
+--
+-- Indexes for table `support`
+--
+ALTER TABLE `support`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `FK_support_user_id` (`user_id`);
+
+--
+-- Indexes for table `tipestorage`
+--
+ALTER TABLE `tipestorage`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `tipeuser`
+--
+ALTER TABLE `tipeuser`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `purchase`
+--
+ALTER TABLE `purchase`
+  MODIFY `id` bigint(20) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `storage`
+--
+ALTER TABLE `storage`
+  MODIFY `id` bigint(20) NOT NULL AUTO_INCREMENT;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `detailitem`
+--
+ALTER TABLE `detailitem`
+  ADD CONSTRAINT `FK_detailitem_instalation_id` FOREIGN KEY (`instalation_id`) REFERENCES `instalation` (`id`) ON DELETE CASCADE,
+  ADD CONSTRAINT `FK_detailitem_itemstatus_id` FOREIGN KEY (`itemstatus_id`) REFERENCES `itemstatus` (`id`) ON DELETE CASCADE;
+
+--
+-- Constraints for table `instalation`
+--
+ALTER TABLE `instalation`
+  ADD CONSTRAINT `FK_instalation_client_id` FOREIGN KEY (`client_id`) REFERENCES `client` (`id`) ON DELETE CASCADE,
+  ADD CONSTRAINT `FK_instalation_package_id` FOREIGN KEY (`package_id`) REFERENCES `package` (`id`) ON DELETE CASCADE,
+  ADD CONSTRAINT `FK_instalation_statusinstalation_id` FOREIGN KEY (`statusinstalation_id`) REFERENCES `statusinstalation` (`id`) ON DELETE CASCADE,
+  ADD CONSTRAINT `FK_instalation_storage_id` FOREIGN KEY (`storage_id`) REFERENCES `storage` (`id`) ON DELETE CASCADE,
+  ADD CONSTRAINT `FK_instalation_support_id` FOREIGN KEY (`support_id`) REFERENCES `support` (`id`) ON DELETE CASCADE;
+
+--
+-- Constraints for table `orders`
+--
+ALTER TABLE `orders`
+  ADD CONSTRAINT `FK_orders_requests_id` FOREIGN KEY (`requests_id`) REFERENCES `requests` (`id`) ON DELETE CASCADE;
+
+--
+-- Constraints for table `purchase`
+--
+ALTER TABLE `purchase`
+  ADD CONSTRAINT `FK_purchase_item_id` FOREIGN KEY (`item_id`) REFERENCES `item` (`id`) ON DELETE CASCADE,
+  ADD CONSTRAINT `FK_purchase_orders_id` FOREIGN KEY (`orders_id`) REFERENCES `orders` (`id`) ON DELETE CASCADE,
+  ADD CONSTRAINT `FK_purchase_statusorder_id` FOREIGN KEY (`statusorders_id`) REFERENCES `statusorders` (`id`) ON DELETE CASCADE;
+
+--
+-- Constraints for table `requests`
+--
+ALTER TABLE `requests`
+  ADD CONSTRAINT `FK_requests_item_id` FOREIGN KEY (`item_id`) REFERENCES `item` (`id`) ON DELETE CASCADE,
+  ADD CONSTRAINT `FK_requests_support_id` FOREIGN KEY (`support_id`) REFERENCES `support` (`id`) ON DELETE CASCADE;
+
+--
+-- Constraints for table `storage`
+--
+ALTER TABLE `storage`
+  ADD CONSTRAINT `FK_storage_item_id` FOREIGN KEY (`item_id`) REFERENCES `item` (`id`) ON DELETE CASCADE,
+  ADD CONSTRAINT `FK_storage_purchase_id` FOREIGN KEY (`purchase_id`) REFERENCES `purchase` (`id`) ON DELETE CASCADE,
+  ADD CONSTRAINT `FK_storage_tipestorage_id` FOREIGN KEY (`tipestorage_id`) REFERENCES `tipestorage` (`id`) ON DELETE CASCADE;
+
+--
+-- Constraints for table `support`
+--
+ALTER TABLE `support`
+  ADD CONSTRAINT `FK_support_user_id` FOREIGN KEY (`user_id`) REFERENCES `tipeuser` (`id`) ON DELETE CASCADE;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
