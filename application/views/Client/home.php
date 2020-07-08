7b2c7890efab20d9c99dc6566f06f517c2f9533b
djendraa <57910787+djendraa@users.noreply.github.com>
2020-07-08 14:07:52 +0700

    07 Juli 2020

--- application/views/Client/home.php
+++ application/views/Client/home.php
@@ -3,7 +3,7 @@
           <div class="">
             <div class="page-title">
               <div class="title_left">
-                <h3>Users <small>Some examples to get you started</small></h3>
+                <h3>CLIENT LIST</h3>
               </div>
 
               <div class="title_right">
@@ -24,7 +24,7 @@
               <div class="col-md-12 col-sm-12 ">
                 <div class="x_panel">
                   <div class="x_title">
-                    <h2>Button Example <small>Users</small></h2>
+                    <a class="fas fa-user-plus" href="#" > CLIENT</a>
                     <ul class="nav navbar-right panel_toolbox">
                       <li><a class="collapse-link"><i class="fa fa-chevron-up"></i></a>
                       </li>
@@ -44,20 +44,17 @@
                       <div class="row">
                           <div class="col-sm-12">
                             <div class="card-box table-responsive">
-                    <p class="text-muted font-13 m-b-30">
-                      The Buttons extension for DataTables provides a common set of options, API methods and styling to display buttons on a page that will interact with a DataTable. The core library provides the based framework upon which plug-ins can built.
-                    </p>
                     <table id="datatable-buttons" class="table table-striped table-bordered" style="width:100%">
-                      <thead>
+                      <thead align="center">
                         <tr>
                           <th>No</th>
                           <th>Client Name</th>
                           <th>Address</th>
                           <th>Contact</th>
-                          <th>Action</th>
+                          <th colspan="2">Action</th>
                         </tr>
                       </thead>
-                      <tbody>
+                      <tbody align="center">
                       <?php 
                       $no = 1;
                       foreach ($dataClient as $d) { ?>
@@ -67,10 +64,10 @@
                           <td><?php echo $d->address ?></td>
                           <td><?php echo $d->contact ?></td>
                           <td>
-                          <a class="rounded-circle btn btn-dark text-white btn-sm" data-placement="top" title="Update"  data-toggle="modal" data-target="#UpdateModal" style="text-decoration: none;">
-                          <i class="fas fa-trash"></i>
+                          <a class="fas fa-edit" href="#" style="text-decoration: none;">
                           </a>
                           </td>
+                          <td><a class="fas fa-trash" href="#" style="text-decoration: none;"></a></td>
                         </tr>
                       <?php } ?>
                       </tbody>
--- application/views/Template/Header.php
+++ application/views/Template/Header.php
@@ -6,6 +6,7 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    <script src="https://kit.fontawesome.com/55d023294a.js" crossorigin="anonymous"></script>
 	<link rel="icon" href="images/favicon.ico" type="image/ico" />
 
     <title>Gentelella Alela! | </title>
