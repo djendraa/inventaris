187a99d94a6c1edd9bea4d9cf6b00f6d5178640a
djendraa <57910787+djendraa@users.noreply.github.com>
2020-07-14 14:54:46 +0700

    14/07/2020

--- application/views/Client/home.php
+++ application/views/Client/home.php
@@ -43,14 +43,14 @@
                       <div class="row">
                           <div class="col-sm-12">
                             <div class="card-box table-responsive">
-                    <table id="datatable-buttons" class="table table-striped table-bordered" style="width:100%">
+                    <table id="datatable" class="table table-striped table-bordered" style="width:100%">
                       <thead align="center">
                         <tr>
                           <th>No</th>
                           <th>Client Name</th>
                           <th>Address</th>
                           <th>Contact</th>
-                          <th colspan="3">Action</th>
+                          <th>Action</th>
                         </tr>
                       </thead>
                       <tbody align="center">
@@ -66,15 +66,11 @@
                           <td><?php echo $d->clientname ?></td>
                           <td><?php echo $d->address ?></td>
                           <td><?php echo $d->contact ?></td>
-                          <td>
-                            <a class="fas fa-user-plus" data-placement="top" title="Add" data-toggle="modal" data-target="#AddModal" style="text-decoration: none;"></a>
-                          </td>
                           <?php include('inc/update.php') ?>
-                          <td>
-                            <a class="fas fa-edit" data-placement="top" title="Update" data-toggle="modal" data-target="#UpdateModal<?php echo $d->id ?>" style="text-decoration: none;">></a>
-                          </td>
                           <?php include('inc/delete.php') ?>
                           <td>
+                            <a class="fas fa-user-plus" data-placement="top" title="Add" data-toggle="modal" data-target="#AddModal" style="text-decoration: none;"></a> &nbsp; &nbsp;
+                            <a class="fas fa-edit" data-placement="top" title="Update" data-toggle="modal" data-target="#UpdateModal<?php echo $d->id ?>" style="text-decoration: none;"></a> &nbsp; &nbsp;
                             <a class="fas fa-trash" data-placement="top" title="Delete" data-toggle="modal" data-target="#DeleteModal<?php echo $d->id ?>" style="text-decoration: none;"></a>
                           </td>
                         </tr>
